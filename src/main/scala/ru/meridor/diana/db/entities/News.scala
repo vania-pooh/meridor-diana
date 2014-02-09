74c308ebba3ae062ca66fea3536e4dbc9d03f9a2
Ivan Krutov <vania-pooh@vania-pooh.com>
2014-02-09 09:02:18 +0400

    MWS-62: added News class and Formatters methods for /news page

---
+++ src/main/scala/ru/meridor/diana/db/entities/News.scala
@@ -0,0 +1,34 @@
+package ru.meridor.diana.db.entities
+
+import ru.meridor.diana.db.DB
+import scala.slick.driver.PostgresDriver.simple._
+import scala.slick.session.Database.threadLocalSession
+import java.sql.Timestamp
+
+/**
+ * Stores entities related to news
+ */
+case class News(date: Timestamp, markdown: String){
+  def this(record: (Timestamp, String)) = this(record._1, record._2)
+}
+
+object News {
+
+  /**
+   * Returns all news in the table
+   * @return
+   */
+  def get: List[News] = {
+    DB withSession {
+      val arr = scala.collection.mutable.ArrayBuffer[News]()
+      val rawRecords = for {
+        n <- ru.meridor.diana.db.tables.News
+      } yield (n.date, n.markdown)
+      rawRecords.sortBy(_._1.desc).list foreach {
+        r => arr += new News(r)
+      }
+      arr.toList
+    }
+  }
+
+}
---
+++ src/main/scala/ru/meridor/diana/util/Formatters.scala
@@ -0,0 +1,22 @@
+package ru.meridor.diana.util
+
+import java.sql.Timestamp
+import java.text.SimpleDateFormat
+import java.util.Date
+
+/**
+ * Contains various formatters
+ */
+object Formatters {
+
+  val DATETIME_FORMAT = "dd.MM.yyyy HH:mm:ss"
+
+  val DATE_FORMAT = "dd.MM.yyyy"
+
+  def timestampToDatetime(ts: Long): String = formatTimestamp(ts, DATETIME_FORMAT)
+
+  def timestampToDate(ts: Long): String = formatTimestamp(ts, DATE_FORMAT)
+
+  private def formatTimestamp(ts: Long, fmt: String) = new SimpleDateFormat(fmt).format(new Date(ts))
+
+}
